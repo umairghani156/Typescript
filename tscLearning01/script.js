7271c0f2a723a3f24e856c84f96bbaf203e7d11c
umairghani156 <umairghani156@gmail.com>
2024-08-20 20:00:20 +0500

    added

--- tscLearning01/script.js
+++ tscLearning01/script.js
@@ -1,6 +1,12 @@
 var a = "Umair";
 console.log(a);
-function hello(a, b) {
-    return a * b;
-}
-hello(2, 3);
+// function hello(a:number, b: number): number{
+//     return a * b 
+// }
+// hello(2,3)
+var n = 123;
+var b = "Umair";
+var func = function (m, n) {
+    return m + n;
+};
+func(2, 3);
--- tscLearning01/script.ts
+++ tscLearning01/script.ts
@@ -2,9 +2,36 @@ var a = "Umair";
 console.log(a);
 
 
-// function hello(a:number, b: number){
+// function hello(a:number, b: number): number{
 //     return a * b 
 // }
 // hello(2,3)
 
+// let n = <number> 123;
+// let b: string = "Umair"
 
+// const func = (m:number,n:number)=>{
+//     return m + n
+// }
+
+// func(2,3)
+
+// const funcb = (m:number, n:number): number=>{
+//     return m * n
+// }
+// funcb(2,3)
+
+//Array in TypeScript
+//TYPE and INTERFACE
+
+type UserName = string | number | boolean;
+
+let c: UserName = false;
+
+
+type User = (m: number, n: number)=> number;
+
+
+const func3: User = (m, n) => {
+    return m * n
+}
